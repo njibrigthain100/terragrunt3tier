d7172c3330de8c13dca7904646379892ded00d74
njibrigthain100 <85526553+njibrigthain100@users.noreply.github.com>
2024-02-28 09:55:53 -0500

    This is for the update made on some files (#25)
    * creating a pull request (#17)
    
    * pushing
    
    * testing in main
    
    ---------
    
    Co-authored-by: Brigthain kargong <kbrigthain@gmail.com>
    
    * Dev environment
    
    * changing the source code
    
    * changed source again
    
    * changed again
    
    * changed the environment
    
    * changing to new commit
    
    * changed the config state
    
    * change the build method
    
    * merge
    
    * deleting
    
    ---------
    
    Co-authored-by: Brigthain kargong <kbrigthain@gmail.com>

--- Environments/Shiloh/.terragrunt-cache/H8ZBnllVNpeWGlW3RyNW0ceREmQ/Aobm1OWoFzJQJHp9-Zs_ho8chOY/modules/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    key            = "terraform/tfstate.tfstate"
-    bucket         = "dynamiccopy"
-    region         = "us-east-1"
-    profile        = "shilohIT"
-    dynamodb_table = "Terraform"
-  }
-}
--- Environments/Shiloh/terragrunt.hcl
+++ Environments/Shiloh/terragrunt.hcl
@@ -25,7 +25,7 @@ include "env" {
 }
 
 locals  {
-    build_environment = "sit"
+    build_environment = "Prod"
     aws_region = "us-east-1"
     state_bucket = "distributorbk"
     dynamoDB_table = "Terraform"
